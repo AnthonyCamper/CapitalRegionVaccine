14f544973f258e7279be72c898c6cd56248e177a
HappyCamper <33391769+AnthonyCamper@users.noreply.github.com>
2021-03-04 21:29:33 -0500

    Updated Maryland Giant Locations

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ config = {
     "zipcode": "12110",
     "nys_sites": ['SUNY Albany '],
     "alb_sites": ['**Washington Avenue Armory - Albany, Schenectady, Troy'],
-    "cvs_sites": ['ALBANY', 'TROY', 'WYNANTSKILL', 'SARATOGA SPRINGS', 'COLONIE', 'GLENVILLE', 'QUEENSBURY'],
+    "cvs_sites": ['Baltimore','Bethesda','Bowie','Dunkirk','Easton','Eldersburg','Elkton','Frederick','Fulton','Hagerstown','Havre De Grace','Hyattsville','Lanham','Leonardtown','Millersville','New Carrollton','Ocean City','Reisterstown','Silver Spring','Wheaton','White Plains'],
     "han_sites": ['52080','51034','51005', '50992', '50955'],
     "wal_location": '"latitude":42.7477661,"longitude":-73.760537'
 }
